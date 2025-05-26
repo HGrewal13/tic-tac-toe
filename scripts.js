364e9af5735f7020806da97fb40faefeae281006
HGrewal13 <harrysgrewal13@gmail.com>
2025-05-26 13:02:09 -0400

    UI added in

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 
 const GameBoard = function() {
-    let gameBoard = new Array(9);
+    let gameBoard = new Array(9).fill(undefined);
 
     // Returns a copy of the gameBoard so it can't be directly changed
     // console.log for now, but the object will need to be returned to make sure we can update on DOM
@@ -21,13 +21,16 @@ const GameBoard = function() {
 
     // test of actual function
     const insertMark = function(mark, position) {
-        // add a check to make sure the index is empty before inserting
-
+        // checkPosition is run before inserting in the game function
         gameBoard[position] = mark;
         console.log(getBoard());
     }
 
-    return {checkPosition, insertMark, getBoard};
+    const checkFullBoard = function() {
+        return gameBoard.every((space) => space !== undefined);
+    }
+
+    return {checkPosition, insertMark, getBoard, checkFullBoard};
 }
 
 // Define createPlayer factory function
@@ -47,6 +50,7 @@ const game = function(board, p1, p2) {
     let gameOver = false;
     let currentPlayer = p1;
 
+    // tracks the positions the player has inserted a mark in. This will allow to check for win conditions
     let playerPositions = {
         p1: [],
         p2: []
@@ -54,8 +58,13 @@ const game = function(board, p1, p2) {
 
     let turnTransition = function() {
         currentPlayer = currentPlayer === p1 ? p2 : p1;
-        console.log(`It's now ${currentPlayer.getName()}'s turn`);
     }
+
+    let getTurnTransitionMessage = function() {
+        return `It's now ${currentPlayer.getName()}'s turn`;
+    }
+
+    const getCurrentPlayer = () => currentPlayer;
 
     const getTurn = () => console.log(`${currentPlayer.getName()}'s turn`);
 
@@ -68,7 +77,6 @@ const game = function(board, p1, p2) {
             console.log("accepted");
             board.insertMark(currentPlayer.getMark(), position);
             storePlayerPositions(position);
-            // checkWinner();
             if(checkWinner()) {
                 return;
             } else {
@@ -107,22 +115,100 @@ const game = function(board, p1, p2) {
                 // If they do, set gameOver to true
                 if(winCon.every(position => playerPositions[player].includes(position))) {
                     alert(`${player} is the winner`);
+                    if(player === "p1") {
+                        p1.win();
+                    } else if(player === "p2") {
+                        p2.win();
+                    }
                     gameOver = true;
                     return true;
                 }
                 // check for draw condition
             }
         }
+
+        if(!gameOver) {
+            let tieCondition = board.checkFullBoard();
+            if(tieCondition) {
+                gameOver = true;
+                alert("Tie Game");
+            }
+        }
         return false;
     }
 
-    return {getTurn, performTurn, checkWinner};
+    function isGameOver() {
+        if(gameOver) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+
+    return {getTurn, getCurrentPlayer, getTurnTransitionMessage, performTurn, checkWinner, isGameOver};
 
 }
 
+// DOM Manipulation for Game
+
+const UI = function() {
+    const main = document.querySelector("main");
+    const currentTurn = document.querySelector(".currentTurn");
+    const p1Score = document.querySelector(".player1Score");
+    const p2Score = document.querySelector(".player2Score");
+
+    p1Score.textContent = p1.getScore();
+    p2Score.textContent = p2.getScore();
+    
+    currentTurn.textContent = currentGame.getTurnTransitionMessage();
+    // Based on which quadrent the user clicks, save that quadrent's value
+    main.addEventListener("click", (event) => {
+        // Pass that value as the position. parseInt must be used so it can be passed as an index
+        const position = parseInt(event.target.dataset.value);
+        // Set target div to be an "X" or "O" based on whose turn it is
+        event.target.textContent = currentGame.getCurrentPlayer().getMark();
+        // use delay so DOM is updated with mark before running game logic. Alert in checkWinner won't allow the DOM to update otherwise
+        // Update this without timeout once I no longer use alert
+        setTimeout(() => {
+
+            currentGame.performTurn(position);
+
+            // overlay to restrict interaction after game is over
+            if (currentGame.isGameOver()) {
+                const overlay = document.createElement("div");
+                const newGameButton = document.createElement("button");
+
+                overlay.classList.add("overlay");
+                overlay.textContent = "Game Over";
+
+                newGameButton.textContent = "New Game";
+
+                main.appendChild(overlay);
+                overlay.appendChild(newGameButton);
+                main.style.pointerEvents = "none";
+            }
+
+            currentTurn.textContent = currentGame.getTurnTransitionMessage();
+            
+            // Update scores
+            p1Score.textContent = p1.getScore();
+            p2Score.textContent = p2.getScore();
+
+        }, 0);
+    })
+
+
+}
+
+// const dialog = document.querySelector("dialog");
+// dialog.showModal();
 
 // Init Function
+
 const p1 = createPlayer("p1", "X");
 const p2 = createPlayer("p2", "O");
 const board = GameBoard();
-const game1 = game(board, p1, p2);
+const currentGame = game(board, p1, p2);
+const interface = UI();
